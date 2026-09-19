16a7828e2215d96516ebea03ed2b9e2f166aa94b
Cesar Lacerda <cesarlac.lima@gmail.com>
2026-09-18 21:04:31 -0300

    1809

--- src/PizzaTest.java
+++ src/PizzaTest.java
@@ -1,4 +1,5 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -15,6 +16,32 @@ public class PizzaTest {
 
         //Assert
         assertEquals(4, quantos);
+    }
+    
+    @Test
+    public void verificaSePodeAdicionarIngredientesAcima(){
+        //Arrange
+        Pizza pizza = new Pizza();
+        pizza.adicionarIngredientes(6);
+        //Act
+        int quantos = pizza.adicionarIngredientes(4);
+
+        //Assert
+        assertEquals(6, quantos);
+
+    }
+
+    @Test
+    public void verificaValorFinal(){
+        //Arrange
+        Pizza pizza = new Pizza();
+        pizza.adicionarIngredientes(3);
+        //Act
+        double valorFinal = pizza.valorFinal();
+
+        //Assert
+        assertEquals(44, valorFinal, 0.01);
+
     }
     
 }
